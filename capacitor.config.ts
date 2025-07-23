97925c907baa0a0b6a50f72f67e715e6f250b522
Jon Herman <jon.herman@outsystems.com>
2025-07-23 14:12:46 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'github-actions-fastlane-example',
+  webDir: 'dist'
+};
+
+export default config;
